cba1b060accc5f2c823969010fcd51b2cf25340a
M4RK-jarvis <marc.vogt08@gmail.com>
2026-05-07 17:00:56 +0200

    Update login.js

--- jarvis/pages/api/auth/meta/login.js
+++ jarvis/pages/api/auth/meta/login.js
@@ -6,7 +6,7 @@ export default function handler(req, res) {
     return res.status(500).json({ error: 'Variables META_APP_ID ou META_REDIRECT_URI manquantes.' })
   }
 
-  // Instagram API with Instagram Login (nouveaux scopes business)
+  // Instagram API with Instagram Login - scopes business
   const scope = 'instagram_business_basic,instagram_business_manage_insights'
 
   const authUrl = `https://www.instagram.com/oauth/authorize?enable_fb_login=0&force_authentication=1&client_id=${clientId}&redirect_uri=${encodeURIComponent(redirectUri)}&response_type=code&scope=${encodeURIComponent(scope)}`
