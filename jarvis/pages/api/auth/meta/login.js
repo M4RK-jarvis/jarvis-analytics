d381fcee8b5a92252032e350bbc06bfbebbd88da
M4RK-jarvis <marc.vogt08@gmail.com>
2026-05-07 17:11:22 +0200

    Update login.js

--- jarvis/pages/api/auth/meta/login.js
+++ jarvis/pages/api/auth/meta/login.js
@@ -9,7 +9,7 @@ export default function handler(req, res) {
   // Instagram API with Instagram Login - scopes business
   const scope = 'instagram_business_basic,instagram_business_manage_insights'
 
-  const authUrl = `https://www.instagram.com/oauth/authorize?enable_fb_login=0&force_authentication=1&client_id=${clientId}&redirect_uri=${encodeURIComponent(redirectUri)}&response_type=code&scope=${encodeURIComponent(scope)}`
+  const authUrl = `https://www.instagram.com/oauth/authorize?force_reauth=true&client_id=${clientId}&redirect_uri=${encodeURIComponent(redirectUri)}&response_type=code&scope=${encodeURIComponent(scope)}`
 
   res.redirect(authUrl)
 }
