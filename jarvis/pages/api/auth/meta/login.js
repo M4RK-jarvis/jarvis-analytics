d0adb87fd166878c16dbd6d14c116ab530236547
M4RK-jarvis <marc.vogt08@gmail.com>
2026-05-07 16:48:31 +0200

    Update login.js

--- jarvis/pages/api/auth/meta/login.js
+++ jarvis/pages/api/auth/meta/login.js
@@ -6,8 +6,8 @@ export default function handler(req, res) {
     return res.status(500).json({ error: 'Variables META_APP_ID ou META_REDIRECT_URI manquantes.' })
   }
 
-  // Permissions demandées via Facebook Login for Business
-  const scope = 'instagram_business_basic,instagram_business_manage_insights,pages_show_list,business_management'
+  // Permissions via Facebook Login for Business pour accéder à Instagram Business
+  const scope = 'instagram_basic,instagram_manage_insights,pages_show_list,pages_read_engagement,business_management'
 
   const authUrl = `https://www.facebook.com/v21.0/dialog/oauth?client_id=${clientId}&redirect_uri=${encodeURIComponent(redirectUri)}&scope=${scope}&response_type=code`
 
